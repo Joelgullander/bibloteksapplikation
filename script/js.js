65a77a5bfe8c2253a736fc9bed3a15cb45af1115
Joel Gullander <Joel.gullander@hotmail.com>
2013-11-27 00:19:15 +0100

    Grund & struktur för att hämta böcker vid kategori klick och andra småfix.
    Nu till sökfunktion.

--- script/js.js
+++ script/js.js
@@ -1,27 +1,48 @@
-start function(){
-        // Add a div to hold our result table
-        $('<div class="menu"/>').appendTo("#content");
-        
-         $.ajax({
-                        url:"sql",
-                        cache:false,
-                        success:function(data){
-                                // We have recieved the answer
-                                // to our question an array (JSON)
-                                // now render it to HTML
-                                renderResult(data);
-                        },
-                        error:function(errorData){
-                                alert(
-                                  "There seems to be an error:\n"
-                                  + errorData.responseJSON.error
-                                );
-                        }
-                });
-}
-
+function start(){
+	$.getJSON(
+		"data/jsonkategory.php",
+		displayData
+		); 
+	};
+	
 // Render a result for genre as HTML
-function renderResult(data){
+
+function displayData(data){
+	var menu =$('.menu');
+
+	for (var i=0; i < data.length;i++)
+	{
+		$("<li>").val(data[i].c_id).text(data[i].name).appendTo(menu);
+		console.log(data[i].c_name);
+	}
+	addEvents();
+	}; 
+	
+	// Klick events
+function addEvents(){
+	$(".menu").on("click","li", function(){
+    $("li").removeClass('highlight_stay');
+     $(this).addClass('highlight_stay');
+     if ($(this).val() == "1"){
+	     console.log("Lista med böcker från c_id = 1");
+     }
+     if ($(this).val() == "2"){
+	     console.log("Lista med böcker från c_id = 2");
+     }
+     if ($(this).val() == "3"){
+	     console.log("Lista med böcker från c_id = 3");
+     }
+     if ($(this).val() == "4"){
+	     console.log("Lista med böcker från c_id = 4");
+     }
+		})
+		};
+		
+
+$(start);
+
+
+/* function renderResult(data){
         var html = "<ul>";
         // loop through result array
         for(var x = 0; var i = 0; i < data.length; i++ x++){
@@ -35,4 +56,6 @@ function renderResult(data){
 }
 
 
-start();
+start(); 
+
+*/
