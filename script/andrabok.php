41a949068da3ef4272fd3be3cbb4368fe6fe4699
v2000 <vrpr2008@gmail.com>
2013-11-28 12:03:18 +0100

    uppdate andrabok.php - working

--- script/andrabok.php
+++ script/andrabok.php
@@ -3,25 +3,19 @@
 $dns ="mysql:dbname=bookstore;host=localhost";
 $myPDO = new PDO($dns , "root", "");
 
-
-$statement = $myPDO->prepare('INSERT INTO books (book_delivery, stock, price, shelf) 
-	VALUES (?, ?, ?, ?, ?) WHERE isbn=$isbn');
-
-$antal=0;
-
 $isbn =$_POST['isbn'];
 $mydate =$_POST['mydate'];
 $antal =$_POST['antal'];
-$sprice =$_POST['spris'];
+$sprice =$_POST['sprice'];
 $hylla =$_POST['hylla'];
 
-//print_r($category);
-$data = array($mydate, $antal,  $sprice, $hylla);
+$statement = $myPDO->prepare("UPDATE books SET book_delivery = '".$mydate."', stock='".$antal."', price='".$sprice."', shelf='".$hylla."' WHERE isbn='".$isbn."'");                
+
+$data = array($isbn, $mydate, $antal,  $sprice, $hylla);
 
 $statement->execute($data);
 
 // Go back to leverans.html
 header('Location: ../leverans.html');
-
 ?>
 
