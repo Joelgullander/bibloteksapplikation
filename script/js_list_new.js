9af940d578416e0f74fe7e5c3f0357f35cec0c56
v2000 <vrpr2008@gmail.com>
2013-12-03 16:09:51 +0100

    uppdate working

--- script/js_list_new.js
+++ script/js_list_new.js
@@ -1,3 +1,7 @@
+
+
+
+
 function start(){
 // add a input field for the search box
  //   $("form.uppdate").append('<div class="info" id="andrabok"><input type="submit" value="Ändra bok">');
@@ -14,9 +18,6 @@ for (var i = 1; i <5; i++) {
             },
             success:function(data){
                displayData(data);
-               uppdate();
-               //addnybok();
-
             },
             error:function(errorData){
                 //console.log(x.responseJSON);
@@ -27,7 +28,7 @@ for (var i = 1; i <5; i++) {
 }
 };
 $(start);
-
+$(uppdate);
 
 
 function displayData(data){
@@ -37,103 +38,118 @@ function displayData(data){
     {
         $("<option>").val(data[i].c_id).text(data[i].name).appendTo(dropdown);
     }
-    }; 
+}; 
 
 
-    function uppdate(){
+function uppdate(){
+
+
+
  // Add event handler klick to btn #andrebok
-        $("#andrabok").mousedown(function(){
+
+    console.log( $(".andraForm").length);
+    console.log($('body').children().length)
+    $(".andraForm").submit(function(e){
+
+        // do not reload the page
+         e.preventDefault();
+
+        //$("#andrabok").mousedown(function(){
         //$("#andrabok").click(function(){
-        
-            var getIsbn=$("#myisbn").val();
-            var getStock=$("#mystock").val();
-            var getShelf=$("#myshelf").val();
-            var getPrice=$("#myprice").val();
-            var getDate=$("#mydate").val();  
+    
+        var getIsbn=$("#myisbn").val();
+        var getStock=$("#mystock").val();
+        var getShelf=$("#myshelf").val();
+        var getPrice=$("#myprice").val();
+        var getDate=$("#mydate").val();  
+
+                    $.ajax(
+                    {     
+                        url:"sql",
+                        cache:false,
+                        data: {
+                            action: "uppdateStock",
+                            stock: getStock,
+                            isbn: getIsbn
+                        },
+                        success:function(data){
+                            
+                           alert("success uppdateStock");
+                        },
+                        error:function(errorData){
+                            //console.log(x.responseJSON);
+                            alert("It is error:"+errorData.responseJSON.error);
+                    }
+            });
+
+                      $.ajax(
+                    {     
+                        url:"sql",
+                        cache:false,
+                        data: {
+                            action: "uppdateShelf",
+                            shelf: getShelf,
+                            isbn: getIsbn
+                        },
+                        success:function(data){
+                           alert("success uppdateShelf");
+                           
+                        },
+                        error:function(errorData){
+                            //console.log(x.responseJSON);
+                            alert("It is error:"+errorData.responseJSON.error);
+                    }
+            });
 
                         $.ajax(
-                        {     
-                            url:"sql",
-                            cache:false,
-                            data: {
-                                action: "uppdateStock",
-                                stock: getStock,
-                                isbn: getIsbn
-                            },
-                            success:function(data){
-                                
-                               alert("success");
-                               console.log("getStock ",getStock);
-                            },
-                            error:function(errorData){
-                                //console.log(x.responseJSON);
-                                alert("It is error:"+errorData.responseJSON.error);
-                        }
-                });
+                    {     
+                        url:"sql",
+                        cache:false,
+                        data: {
+                            action: "uppdateButicPrice",
+                            price: getPrice,
+                            isbn: getIsbn
+                        },
+                        success:function(data){
+                           alert("success uppdateButicPrice");
+                           
+                        },
+                        error:function(errorData){
+                            //console.log(x.responseJSON);
+                            alert("It is error:"+errorData.responseJSON.error);
+                    }
+            });
 
-                          $.ajax(
-                        {     
-                            url:"sql",
-                            cache:false,
-                            data: {
-                                action: "uppdateShelf",
-                                shelf: getShelf,
-                                isbn: getIsbn
-                            },
-                            success:function(data){
-                               alert("success");
-                               console.log("getShelf ",getShelf);
-                            },
-                            error:function(errorData){
-                                //console.log(x.responseJSON);
-                                alert("It is error:"+errorData.responseJSON.error);
-                        }
-                });
-
-                            $.ajax(
-                        {     
-                            url:"sql",
-                            cache:false,
-                            data: {
-                                action: "uppdateButicPrice",
-                                price: getPrice,
-                                isbn: getIsbn
-                            },
-                            success:function(data){
-                               alert("success");
-                               console.log("getPrice ",getPrice);
-                            },
-                            error:function(errorData){
-                                //console.log(x.responseJSON);
-                                alert("It is error:"+errorData.responseJSON.error);
-                        }
-                });
-
-                                $.ajax(
-                        {     
-                            url:"sql",
-                            cache:false,
-                            data: {
-                                action: "uppdateDate",
-                                book_delivery: getDate,
-                                isbn: getIsbn
-                            },
-                            success:function(data){
-                               alert("success");
-                               console.log("getDate ",getDate);
-                            },
-                            error:function(errorData){
-                                //console.log(x.responseJSON);
-                                alert("It is error:"+errorData.responseJSON.error);
-                        }
-                });
-                         console.log("getIsbn ",getIsbn);
-                   
-        });
+               
+                        $.ajax(
+                    {     
+                        url:"sql",
+                        cache:false,
+                        data: {
+                            action: "uppdateDate",
+                            book_delivery: getDate,
+                            isbn: getIsbn
+                        },
+                        success:function(data){
+                           alert("success uppdateDate");
+                           
+                        },
+                        error:function(errorData){
+                            //console.log(x.responseJSON);
+                            alert("It is error:"+errorData.responseJSON.error);
+                    }
+            });
+                        console.log("getStock ",getStock);
+                        console.log("getShelf ",getShelf);
+                        console.log("getPrice ",getPrice);    
+                        console.log("getDate ",getDate);
+                        console.log("getIsbn ",getIsbn);
+               
+    });
 
        
 };
-
+/*
 function addnybok(){
  // Add event handler klick to btn #andrebok
         $("#addnybok").mousedown(function(){
@@ -192,4 +208,4 @@ function addnybok(){
         });
 
        
-};
+};*/
